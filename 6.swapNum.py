53672a2cbf38caa30ca73847fc68dcc34ba96fe3
DANISH <65593339+khand420@users.noreply.github.com>
2021-10-07 21:47:29 +0530

    Danish

---
+++ 6.swapNum.py
@@ -0,0 +1,27 @@
+
+# using third variable
+# a = int(input("Enter the value of a: "))
+# b = int(input("Enter the value of b: "))
+
+# c = a 
+# a = b
+# b = c
+
+# print("----------------------------")
+
+# print("Value of a After swapping is ", a)
+# print("Value of b After swapping is ", b)
+
+
+# using without third variable
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+
+a= a+b
+b= a-b
+a= a-b
+
+print("----------------------------")
+
+print("Value of a After swapping is ", a)
+print("Value of b After swapping is ", b)
