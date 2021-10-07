e99636fdf4fd82018b9d98d35848e017f052e536
adityasonani <70087338+adityasonani@users.noreply.github.com>
2021-10-07 10:15:54 +0530

    adityasonani-29

---
+++ LCSDP.py
@@ -0,0 +1,18 @@
+def lcs(s1 , s2):
+   m, n = len(s1), len(s2)
+   prev, cur = [0]*(n+1), [0]*(n+1)
+   for i in range(1, m+1):
+       for j in range(1, n+1):
+           if s1[i-1] == s2[j-1]:
+               cur[j] = 1 + prev[j-1]
+           else:
+               if cur[j-1] > prev[j]:
+                   cur[j] = cur[j-1]
+               else:
+                   cur[j] = prev[j]
+       cur, prev = prev, cur
+   return prev[n]
+
+s1 = input()
+s2 = input()
+print("Length of LCS is ", lcs(s1, s2))
